4a4451abb129c0a27641e27770b11f3a3f613e69
Jecky <ht974@nyu.edu>
2018-04-02 20:41:28 -0400

    react app folder structure

---
+++ src/App/App.js
@@ -0,0 +1,24 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Your Favorite Blogs</h1>
+                </header>
+                <div className="container">
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
