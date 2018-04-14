c8199bfe7bb2cfc54d8038e32a059094835d3df8
Jecky <ht974@nyu.edu>
2018-04-13 21:33:43 -0400

    missed file

---
+++ src/ArticlePanel/ArticlePanel.js
@@ -0,0 +1,82 @@
+import './ArticlePanel.css';
+
+import React, {Component} from 'react';
+
+import ArticleItem from '../ArticleItem/ArticleItem';
+
+class ArticlePanel extends Component{
+    constructor() {
+        super();
+        this.state = {articles: null};
+    }
+
+    componentDidMount (){
+        this.loadMoreArticles();
+    }
+
+    loadMoreArticles() {
+        this.setState({
+            articles: [
+                {
+                    'url' : 'http://www.freebuf.com/news/167200.html',
+                    'title' : 'Cisco Devices have Serious Vulnerabilities',
+                    'description' : '思科在其IOS软件中修补了30多个漏洞，其中包括一个严重的远程代码执行漏洞，该漏洞可以对数十万甚至数百万台设备暴露在网络上的设备发起的远程攻击。',
+                    'source' : 'freebuf',
+                    'urlToImage' : 'http://image.3001.net/images/20180402/15226483193095.png',
+                    'digest' : 'ASSSSS',
+                    'reason' : 'Recommended'
+                },
+                {
+                    'url' : 'http://www.freebuf.com/news/167200.html',
+                    'title' : 'Cisco Devices have Serious Vulnerabilities',
+                    'description' : '思科在其IOS软件中修补了30多个漏洞，其中包括一个严重的远程代码执行漏洞，该漏洞可以对数十万甚至数百万台设备暴露在网络上的设备发起的远程攻击。',
+                    'source' : 'freebuf',
+                    'urlToImage' : 'http://image.3001.net/images/20180402/15226483193095.png',
+                    'digest' : 'ASSSSS',
+                    'reason' : 'Recommended',
+                    'time' : 'Today'
+                }
+            ]
+        })
+    }
+
+    renderArticles() {
+        var articles_list = this.state.articles.map( article => {
+            return(
+                <a className='list-group-item' key={article.digest} href='#'>
+                    <ArticleItem article={article}/>
+                </a>
+            )});
+
+        return(
+            <div className='container-fluid'>
+                <div className='list-group'>
+                    { articles_list }
+                </div>
+            </div>
+        );
+    }
+
+    render() {
+        if (this.state.articles)
+        {
+            return(
+                <div>
+                    {this.renderArticles()}
+                </div>
+            );
+        }
+        else
+        {
+            return(
+                <div>
+                    <div id='msg-app-loading'>
+                        Loading
+                    </div>
+                </div>
+            );
+        }
+    }
+}
+
+export default ArticlePanel;
